7718703d94d0e9ac1104e00c1bc41157bd55656d
Bikash Shah <64688350+bikash330@users.noreply.github.com>
2024-03-19 16:01:45 +0545

    Update main.py
    Added a filtering skills code so that person can extract a data related to data skills

--- main.py
+++ main.py
@@ -12,20 +12,21 @@ jobs = soup.find_all('li', {'class': 'clearfix job-bx wht-shd-bx'})
 
 for job in jobs:
     company_name = job.find('h3',{'class':'joblist-comp-name'}).text.strip()
-    data['Name'].append(company_name) #appending the company name into dictionary
     skills = job.find('span',{'class':'srp-skills'}).text.strip()
-    data['Skills'].append(skills) #appending the skills into dictionary
     location = job.find('ul',{'class':'top-jd-dtl clearfix'}).span.text
-    data['Location'].append(location) #appending the location info into dictionary
     more_info = job.header.h2.a['href']
-    data['Link'].append(more_info) #appending the link into dictionary
-
+    
+    #Filtering out the output  based on user entered skills
     if familiar_skill in  skills:
-        print(f'Company Name: {company_name}')
-        print(f'Skills Required: {skills}')
-        print(f'Location: {location}')
-        print(f"More Info: {more_info}")
-        print("\n-----------------------------\n")
+        # print(f'Company Name: {company_name}')
+        data['Name'].append(company_name) #appending the company name into dictionary
+        # print(f'Skills Required: {skills}')
+        data['Skills'].append(skills) #appending the skills into dictionary
+        # print(f'Location: {location}')
+        data['Location'].append(location) #appending the location info into dictionary
+        # print(f"More Info: {more_info}")
+        data['Link'].append(more_info) #appending the link into dictionary
+        # print("\n-----------------------------\n")
 
 #Converting Dictionary to DataFrame and saving it as CSV file
 df = pd.DataFrame.from_dict(data)
